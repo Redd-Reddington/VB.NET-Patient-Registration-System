464fbe6e3191b754e20a050e9f5c235a9d2c8efb
Redd-Reddington <yduose007@outlook.com>
2025-09-20 12:35:49 +0000

    Add files via upload

---
+++ Patient Registration System/Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtFullName = New TextBox()
+        cmbGender = New ComboBox()
+        txtPhoneNumber = New TextBox()
+        txtAddress = New TextBox()
+        txtMedicalCondition = New TextBox()
+        dtpDateOfBirth = New DateTimePicker()
+        dtpRegistrationDate = New DateTimePicker()
+        picProfile = New PictureBox()
+        lblFullName = New Label()
+        lblGender = New Label()
+        lblDateOfBirth = New Label()
+        lblPhoneNumber = New Label()
+        lblAddress = New Label()
+        lblMedicalCondition = New Label()
+        lblRegistrationDate = New Label()
+        lblProfileImage = New Label()
+        lblHeading = New Label()
+        dgvDisplay = New DataGridView()
+        btnBrowse = New Button()
+        btnSave = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnClear = New Button()
+        CType(picProfile, ComponentModel.ISupportInitialize).BeginInit()
+        CType(dgvDisplay, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtFullName
+        ' 
+        txtFullName.Location = New Point(285, 86)
+        txtFullName.Name = "txtFullName"
+        txtFullName.Size = New Size(267, 27)
+        txtFullName.TabIndex = 0
+        ' 
+        ' cmbGender
+        ' 
+        cmbGender.FormattingEnabled = True
+        cmbGender.Items.AddRange(New Object() {"Male", "Female"})
+        cmbGender.Location = New Point(285, 137)
+        cmbGender.Name = "cmbGender"
+        cmbGender.Size = New Size(267, 28)
+        cmbGender.TabIndex = 1
+        ' 
+        ' txtPhoneNumber
+        ' 
+        txtPhoneNumber.Location = New Point(285, 258)
+        txtPhoneNumber.Name = "txtPhoneNumber"
+        txtPhoneNumber.Size = New Size(267, 27)
+        txtPhoneNumber.TabIndex = 3
+        ' 
+        ' txtAddress
+        ' 
+        txtAddress.Location = New Point(285, 308)
+        txtAddress.Name = "txtAddress"
+        txtAddress.Size = New Size(267, 27)
+        txtAddress.TabIndex = 4
+        ' 
+        ' txtMedicalCondition
+        ' 
+        txtMedicalCondition.Location = New Point(285, 364)
+        txtMedicalCondition.Name = "txtMedicalCondition"
+        txtMedicalCondition.Size = New Size(267, 27)
+        txtMedicalCondition.TabIndex = 5
+        ' 
+        ' dtpDateOfBirth
+        ' 
+        dtpDateOfBirth.Location = New Point(285, 194)
+        dtpDateOfBirth.Name = "dtpDateOfBirth"
+        dtpDateOfBirth.Size = New Size(267, 27)
+        dtpDateOfBirth.TabIndex = 6
+        ' 
+        ' dtpRegistrationDate
+        ' 
+        dtpRegistrationDate.Location = New Point(285, 435)
+        dtpRegistrationDate.Name = "dtpRegistrationDate"
+        dtpRegistrationDate.Size = New Size(267, 27)
+        dtpRegistrationDate.TabIndex = 7
+        ' 
+        ' picProfile
+        ' 
+        picProfile.Location = New Point(285, 497)
+        picProfile.Name = "picProfile"
+        picProfile.Size = New Size(267, 181)
+        picProfile.TabIndex = 8
+        picProfile.TabStop = False
+        ' 
+        ' lblFullName
+        ' 
+        lblFullName.AutoSize = True
+        lblFullName.Font = New Font("Segoe UI", 12F)
+        lblFullName.Location = New Point(98, 85)
+        lblFullName.Name = "lblFullName"
+        lblFullName.Size = New Size(100, 28)
+        lblFullName.TabIndex = 9
+        lblFullName.Text = "Full Name"
+        ' 
+        ' lblGender
+        ' 
+        lblGender.AutoSize = True
+        lblGender.Font = New Font("Segoe UI", 12F)
+        lblGender.Location = New Point(98, 133)
+        lblGender.Name = "lblGender"
+        lblGender.Size = New Size(76, 28)
+        lblGender.TabIndex = 10
+        lblGender.Text = "Gender"
+        ' 
+        ' lblDateOfBirth
+        ' 
+        lblDateOfBirth.AutoSize = True
+        lblDateOfBirth.Font = New Font("Segoe UI", 12F)
+        lblDateOfBirth.Location = New Point(98, 194)
+        lblDateOfBirth.Name = "lblDateOfBirth"
+        lblDateOfBirth.Size = New Size(125, 28)
+        lblDateOfBirth.TabIndex = 11
+        lblDateOfBirth.Text = "Date Of Birth"
+        ' 
+        ' lblPhoneNumber
+        ' 
+        lblPhoneNumber.AutoSize = True
+        lblPhoneNumber.Font = New Font("Segoe UI", 12F)
+        lblPhoneNumber.Location = New Point(98, 257)
+        lblPhoneNumber.Name = "lblPhoneNumber"
+        lblPhoneNumber.Size = New Size(144, 28)
+        lblPhoneNumber.TabIndex = 13
+        lblPhoneNumber.Text = "Phone Number"
+        ' 
+        ' lblAddress
+        ' 
+        lblAddress.AutoSize = True
+        lblAddress.Font = New Font("Segoe UI", 12F)
+        lblAddress.Location = New Point(98, 308)
+        lblAddress.Name = "lblAddress"
+        lblAddress.Size = New Size(82, 28)
+        lblAddress.TabIndex = 14
+        lblAddress.Text = "Address"
+        ' 
+        ' lblMedicalCondition
+        ' 
+        lblMedicalCondition.AutoSize = True
+        lblMedicalCondition.Font = New Font("Segoe UI", 12F)
+        lblMedicalCondition.Location = New Point(98, 364)
+        lblMedicalCondition.Name = "lblMedicalCondition"
+        lblMedicalCondition.Size = New Size(173, 28)
+        lblMedicalCondition.TabIndex = 15
+        lblMedicalCondition.Text = "Medical Condition"
+        ' 
+        ' lblRegistrationDate
+        ' 
+        lblRegistrationDate.AutoSize = True
+        lblRegistrationDate.Font = New Font("Segoe UI", 12F)
+        lblRegistrationDate.Location = New Point(98, 433)
+        lblRegistrationDate.Name = "lblRegistrationDate"
+        lblRegistrationDate.Size = New Size(163, 28)
+        lblRegistrationDate.TabIndex = 16
+        lblRegistrationDate.Text = "Registration Date"
+        ' 
+        ' lblProfileImage
+        ' 
+        lblProfileImage.AutoSize = True
+        lblProfileImage.Font = New Font("Segoe UI", 12F)
+        lblProfileImage.Location = New Point(98, 535)
+        lblProfileImage.Name = "lblProfileImage"
+        lblProfileImage.Size = New Size(137, 28)
+        lblProfileImage.TabIndex = 17
+        lblProfileImage.Text = "Profile Imagee"
+        ' 
+        ' lblHeading
+        ' 
+        lblHeading.AutoSize = True
+        lblHeading.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblHeading.ForeColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        lblHeading.Location = New Point(285, 9)
+        lblHeading.Name = "lblHeading"
+        lblHeading.Size = New Size(547, 46)
+        lblHeading.TabIndex = 18
+        lblHeading.Text = "PATIENT REGISTRATION SYSTEM"
+        ' 
+        ' dgvDisplay
+        ' 
+        dgvDisplay.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvDisplay.Location = New Point(625, 86)
+        dgvDisplay.Name = "dgvDisplay"
+        dgvDisplay.RowHeadersWidth = 51
+        dgvDisplay.Size = New Size(1154, 564)
+        dgvDisplay.TabIndex = 19
+        ' 
+        ' btnBrowse
+        ' 
+        btnBrowse.Location = New Point(330, 684)
+        btnBrowse.Name = "btnBrowse"
+        btnBrowse.Size = New Size(160, 29)
+        btnBrowse.TabIndex = 20
+        btnBrowse.Text = "Browse"
+        btnBrowse.UseVisualStyleBackColor = True
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(149, 741)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(122, 43)
+        btnSave.TabIndex = 21
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(293, 741)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(122, 43)
+        btnUpdate.TabIndex = 22
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(445, 741)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(122, 43)
+        btnDelete.TabIndex = 23
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(585, 741)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(122, 43)
+        btnClear.TabIndex = 24
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(1791, 907)
+        Controls.Add(btnClear)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnSave)
+        Controls.Add(btnBrowse)
+        Controls.Add(dgvDisplay)
+        Controls.Add(lblHeading)
+        Controls.Add(lblProfileImage)
+        Controls.Add(lblRegistrationDate)
+        Controls.Add(lblMedicalCondition)
+        Controls.Add(lblAddress)
+        Controls.Add(lblPhoneNumber)
+        Controls.Add(lblDateOfBirth)
+        Controls.Add(lblGender)
+        Controls.Add(lblFullName)
+        Controls.Add(picProfile)
+        Controls.Add(dtpRegistrationDate)
+        Controls.Add(dtpDateOfBirth)
+        Controls.Add(txtMedicalCondition)
+        Controls.Add(txtAddress)
+        Controls.Add(txtPhoneNumber)
+        Controls.Add(cmbGender)
+        Controls.Add(txtFullName)
+        Name = "Form1"
+        Text = "Form1"
+        CType(picProfile, ComponentModel.ISupportInitialize).EndInit()
+        CType(dgvDisplay, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtFullName As TextBox
+    Friend WithEvents cmbGender As ComboBox
+    Friend WithEvents txtPhoneNumber As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtMedicalCondition As TextBox
+    Friend WithEvents dtpDateOfBirth As DateTimePicker
+    Friend WithEvents dtpRegistrationDate As DateTimePicker
+    Friend WithEvents picProfile As PictureBox
+    Friend WithEvents lblFullName As Label
+    Friend WithEvents lblGender As Label
+    Friend WithEvents lblDateOfBirth As Label
+    Friend WithEvents lblPhoneNumber As Label
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents lblMedicalCondition As Label
+    Friend WithEvents lblRegistrationDate As Label
+    Friend WithEvents lblProfileImage As Label
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents dgvDisplay As DataGridView
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+
+End Class
---
+++ Patient Registration System/Form1.vb
@@ -0,0 +1,211 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+
+Public Class Form1
+
+    Dim connString As String = "Data Source=.\SQLEXPRESS;Initial Catalog=PatientDB;Integrated Security=True"
+    Dim selectedImageBytes As Byte()
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        'Insert new records into the database
+        Using conn As New SqlConnection(connString)
+            Dim cmd As New SqlCommand("INSERT INTO Patients (FullName, Gender, DateOfBirth, PhoneNumber, Address, MedicalCondition, RegistrationDate, ProfileImage) VALUES (@FullName, @Gender, @DOB, @Phone, @Address, @MedicalCondition, @RegDate, @ProfileImage)", conn)
+
+            cmd.Connection = conn
+            cmd.Parameters.AddWithValue("@FullName", txtFullName.Text)
+            cmd.Parameters.AddWithValue("@Gender", cmbGender.SelectedItem.ToString())
+            cmd.Parameters.AddWithValue("@DOB", dtpDateOfBirth.Value.Date)
+            cmd.Parameters.AddWithValue("@Phone", txtPhoneNumber.Text)
+            cmd.Parameters.AddWithValue("@Address", txtAddress.Text)
+            cmd.Parameters.AddWithValue("@MedicalCondition", If(String.IsNullOrWhiteSpace(txtMedicalCondition.Text), DBNull.Value, txtMedicalCondition.Text))
+            cmd.Parameters.AddWithValue("@RegDate", dtpRegistrationDate.Value.Date)
+            cmd.Parameters.Add("@ProfileImage", SqlDbType.VarBinary).Value = If(selectedImageBytes IsNot Nothing, selectedImageBytes, DBNull.Value)
+
+            conn.Open()
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Patient Registered Successfully!")
+
+            DisplayRecords()
+
+        End Using
+
+
+    End Sub
+
+
+    'Display records on gridview
+
+    Private Sub DisplayRecords()
+
+        Using conn As New SqlConnection(connString)
+
+            Dim cmd As New SqlCommand("SELECT * FROM Patients", conn)
+            Dim adapter As New SqlDataAdapter(cmd)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+            dgvDisplay.DataSource = table
+
+        End Using
+
+
+    End Sub
+
+    'Resize image to fit in the picture box
+    Private Function ResizeImage(ByVal img As Image, ByVal width As Integer, ByVal height As Integer) As Image
+        Dim resizedBitmap As New Bitmap(width, height)
+        Using g As Graphics = Graphics.FromImage(resizedBitmap)
+            g.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+            g.DrawImage(img, 0, 0, width, height)
+        End Using
+        Return resizedBitmap
+    End Function
+
+
+    'To load image for PC
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+
+        Using ofd As New OpenFileDialog
+
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+            If ofd.ShowDialog() = DialogResult.OK Then
+
+                Using ms As New MemoryStream(File.ReadAllBytes(ofd.FileName))
+
+                    Dim originalImage As Image = Image.FromStream(ms)
+                    picProfile.Image = ResizeImage(originalImage, 200, 120) ' Resize to 150x150 pixels
+                    selectedImageBytes = ms.ToArray()
+
+                End Using
+
+            End If
+
+        End Using
+
+    End Sub
+
+
+    'Update records in database
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+
+        If dgvDisplay.SelectedRows.Count > 0 Then
+            Dim id = Convert.ToInt32(dgvDisplay.SelectedRows(0).Cells("Id").Value)
+            Using conn As New SqlConnection(connString)
+                Dim cmd As New SqlCommand("UPDATE Patients SET FullName = @FullName, Gender = @Gender, DateOfBirth = @DOB, PhoneNumber = @Phone, Address = @Address, MedicalCondition = @MedicalCondition, RegistrationDate = @RegDate, ProfileImage = @ProfileImage WHERE Id = @Id", conn)
+                cmd.Connection = conn
+                cmd.Parameters.AddWithValue("@Id", id)
+                cmd.Parameters.AddWithValue("@FullName", txtFullName.Text)
+                cmd.Parameters.AddWithValue("@Gender", cmbGender.SelectedItem.ToString())
+                cmd.Parameters.AddWithValue("@DOB", dtpDateOfBirth.Value.Date)
+                cmd.Parameters.AddWithValue("@Phone", txtPhoneNumber.Text)
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text)
+                cmd.Parameters.AddWithValue("@MedicalCondition", If(String.IsNullOrWhiteSpace(txtMedicalCondition.Text), DBNull.Value, txtMedicalCondition.Text))
+                cmd.Parameters.AddWithValue("@RegDate", dtpRegistrationDate.Value.Date)
+                cmd.Parameters.Add("@ProfileImage", SqlDbType.VarBinary).Value = If(selectedImageBytes IsNot Nothing, selectedImageBytes, DBNull.Value)
+                conn.Open()
+                cmd.ExecuteNonQuery()
+
+                MessageBox.Show("Patient Record Updated Successfully!")
+
+                DisplayRecords()
+
+            End Using
+
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisplayRecords()
+
+        btnDelete.Enabled = False
+        btnUpdate.Enabled = False
+        btnClear.Enabled = False
+
+    End Sub
+
+    Private Sub dgvDisplay_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDisplay.CellClick
+
+        'If header is clicked, no record should display
+        If e.RowIndex < 0 OrElse dgvDisplay.Rows(e.RowIndex).IsNewRow Then Exit Sub
+
+        Dim row As DataGridViewRow = dgvDisplay.Rows(e.RowIndex)
+
+        txtFullName.Text = row.Cells("FullName").Value.ToString()
+        cmbGender.SelectedItem = row.Cells("Gender").Value.ToString()
+        dtpDateOfBirth.Value = Convert.ToDateTime(row.Cells("DateOfBirth").Value)
+        txtPhoneNumber.Text = row.Cells("PhoneNumber").Value.ToString()
+        txtAddress.Text = row.Cells("Address").Value.ToString()
+        txtMedicalCondition.Text = If(row.Cells("MedicalCondition").Value IsNot DBNull.Value, row.Cells("MedicalCondition").Value.ToString(), "")
+        dtpRegistrationDate.Value = Convert.ToDateTime(row.Cells("RegistrationDate").Value)
+
+        btnUpdate.Enabled = True
+        btnDelete.Enabled = True
+        btnClear.Enabled = True
+
+        'display image
+
+        If Not IsDBNull(row.Cells("ProfileImage").Value) Then
+
+            Dim imageBytes As Byte() = CType(row.Cells("ProfileImage").Value, Byte())
+
+            Using ms As New MemoryStream(imageBytes)
+                Dim originalImage As Image = Image.FromStream(ms)
+                picProfile.Image = ResizeImage(originalImage, 250, 170) ' Resize before display
+            End Using
+
+        Else
+            picProfile.Image = Nothing
+
+        End If
+
+    End Sub
+
+
+
+    'Delete records from database
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        If dgvDisplay.SelectedRows.Count > 0 Then
+            Dim id = Convert.ToInt32(dgvDisplay.SelectedRows(0).Cells("Id").Value)
+            Using conn As New SqlConnection(connString)
+                Dim cmd As New SqlCommand("DELETE FROM Patients WHERE Id = @Id", conn)
+                cmd.Connection = conn
+                cmd.Parameters.AddWithValue("@Id", id)
+                conn.Open()
+                cmd.ExecuteNonQuery()
+
+                MessageBox.Show("Patient Record Deleted Successfully!")
+
+                DisplayRecords()
+
+            End Using
+
+        End If
+
+    End Sub
+
+    'Clear all fields
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        txtFullName.Clear()
+        cmbGender.SelectedIndex = -1
+        dtpDateOfBirth.Value = DateTime.Now
+        txtPhoneNumber.Clear()
+        txtAddress.Clear()
+        txtMedicalCondition.Clear()
+        dtpRegistrationDate.Value = DateTime.Now
+        picProfile.Image = Nothing
+        btnUpdate.Enabled = False
+        btnDelete.Enabled = False
+        btnClear.Enabled = False
+
+
+    End Sub
+
+
+End Class
